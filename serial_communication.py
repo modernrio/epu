abb20ef240c5824d064ed777cfd0aac1b67adca0
Markus Schneider <markus@icet-clan.de>
2017-03-19 17:02:15 +0100

    Cleanup serial communication

--- serial_communication.py
+++ serial_communication.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
-""" Serial communication """
 
-import time
+from pynput.keyboard import Key, Listener
 import serial
-import sys
+import time
 
-ser = serial.serial_for_url("spy:///dev/ttyUSB1?file=test.txt", timeout=1, baudrate=115200)
+ser = serial.serial_for_url("spy:///dev/ttyUSB1?file=uart_log.txt", timeout=1, baudrate=9600)
 
-time.sleep(1)
+def on_press(key):
+    print('{0} pressed'.format(key))
+    if len(str(key)) == 3:
+        code = str(key)[1]
+        ser.write(str.encode(code))
+    elif str(key) == "Key.enter":
+        ser.write(str.encode("\n"))
+    elif str(key) == "Key.backspace":
+        ser.write(str.encode("\b"))
+    elif str(key) == "Key.space":
+        ser.write(str.encode(" "))
+    time.sleep(0.01)
 
-value = b""
-msg = ""
-while True:
-    size = 1
-    cmd = str.encode(input(">> "))
-    size = len(cmd)
-    if cmd == b"exit":
-        sys.exit()
-    for char in str(cmd)[2:-1]:
-        print(char)
-        ser.write(str.encode(char))
-        time.sleep(0.001)
-    # value = ser.read(size)
-    if value != b"":
-        try:
-            print(value.decode("ascii"))
-        except:
-            pass
-print("\n")
+def on_release(key):
+    # print('{0} release'.format(key))
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
